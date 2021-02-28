84d0d9ae6428438d9369f485bb9710ec9f0651a7
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-28 13:43:56 +0300

    Initial Commit

---
+++ HW4-1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW4-1
+//
+//  Created by Alyaev Roman on 28.02.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

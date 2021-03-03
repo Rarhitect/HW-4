bed3381149c1f2fd643abdadf18dbd098e3c907d
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-03-03 20:22:59 +0300

    Пункт 1

--- HW4-1/main.cpp
+++ HW4-1/main.cpp
@@ -6,8 +6,15 @@
 //
 
 #include <iostream>
+#include <random>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
+
+int main(int argc, const char * argv[])
+{
+    //создадим последовательность П1 из чисел от 1 до 10
+    std::vector < int > P1 {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
     
     return 0;
 }

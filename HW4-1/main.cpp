ce3b0d158c102e6712bd341f2ff4e975f454fa6c
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-03-03 20:26:57 +0300

    Пункт 2

--- HW4-1/main.cpp
+++ HW4-1/main.cpp
@@ -15,6 +15,12 @@ int main(int argc, const char * argv[])
     //создадим последовательность П1 из чисел от 1 до 10
     std::vector < int > P1 {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     
+    //добавим несколько чисел в П1 через cin
+    int a, b, c, d;
+    std::cout << "Let's add some numbers into P1: ";
+    std::cin >> a >> b >> c >> d;
+    std::cout << std::endl;
+    
     
     return 0;
 }

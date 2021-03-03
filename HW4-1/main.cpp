c6a46697604fdc94091b958efc0fa0d4256f292a
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-03-03 22:14:24 +0300

    More new tasks

--- HW4-1/main.cpp
+++ HW4-1/main.cpp
@@ -8,12 +8,37 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <algorithm>
 
 void print_vector(std::vector <int> vector)
 {
     for (auto &i: vector)
         std::cout << i << " ";
     std::cout << std::endl;
+}
+
+bool predicate (int a)
+{
+    if (a / 2 == 0)
+        return 1;
+    else
+        return 0;
+}
+
+bool is_prime(int x)
+{
+    std::vector < int > v;
+    if (x == 1)
+    {
+        return false;
+    }
+    for (auto i = 2; i < x; ++i)
+    {
+        v.push_back(x % i);
+    }
+    auto count_of_zeroes = std::count_if(std::begin(v), std::end(v), [](auto x){return x == 0;});
+    
+    return (count_of_zeroes == 0);
 }
 
 int main(int argc, const char * argv[])
@@ -38,6 +63,25 @@ int main(int argc, const char * argv[])
     print_vector(P1);
     
     //удалим дубликаты П1
+    std::sort(std::begin(P1), std::end(P1));
+    P1.erase(std::unique(std::begin(P1), std::end(P1)), std::end(P1));
+    print_vector(P1);
+    
+    //подсчитаем количество четных чисел в П1
+    auto even = std::count_if(std::begin(P1), std::end(P1), [](auto x){return x % 2 == 0;});
+    std::cout << even << " evens" << std::endl;
+    
+    //определим максимальное и минимальное в П1
+    auto [min, max] = std::minmax_element(std::begin(P1), std::end(P1));
+    std::cout << "Min num = " << *min << "; Max num = " << *max << std::endl;
+    
+    //попробуем найти хотя бы одно простое число в П1
+    auto prime_num = std::find_if(std::begin(P1), std::end(P1), [](auto x){return is_prime(x);});
+    if (prime_num == std::end(P1))
+        std::cout << "No prime numbers" << std::endl;
+    else
+        std::cout << "There is at least one prime num: " << *prime_num << std::endl;
+    
     
     return 0;
 }
